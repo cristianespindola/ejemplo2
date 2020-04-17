de00784e0eeccc78bf3e4e9bcadb3754a5bdbf34
Cristian <cris.esroj@gmail.com>
2020-04-17 11:51:35 -0300

    prueba1q

--- src/test/java/ar/edu/unq/ejemplo2/Ejemplo2ApplicationTests.java
+++ src/test/java/ar/edu/unq/ejemplo2/Ejemplo2ApplicationTests.java
@@ -1,5 +1,7 @@
 package ar.edu.unq.ejemplo2;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,8 @@ class Ejemplo2ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat("hola")
+	      .isEqualTo("hola");
 	}
 
 }
