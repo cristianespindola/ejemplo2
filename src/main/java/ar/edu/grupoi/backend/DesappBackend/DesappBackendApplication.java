60bb4fec41e773fd0238b9c6be914016bbbfb56e
Cristian <cris.esroj@gmail.com>
2020-09-08 23:25:13 -0300

    firts project

---
+++ src/main/java/ar/edu/grupoi/backend/DesappBackend/DesappBackendApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.grupoi.backend.DesappBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/grupoi/backend/DesappBackend/DesappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.grupoi.backend.DesappBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
